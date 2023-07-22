d197893fd67e4855fd57fdb565c59cb089906c72
Lelo <heloisacmk@hotmail.com>
2023-07-22 09:55:05 -0700

    fetcher component start

---
+++ app/components/verse-fetcher.jsx
@@ -0,0 +1,24 @@
+const RANDOM_QUOTE_URL = "https://inspo-quotes-api.herokuapp.com/quotes/random";
+import { useState, useEffect } from "react";
+
+export default function VerseFetcher () {
+    const [quote, setQuote] = useState({});
+    
+    async function fetchQuote () {
+        const response = await fetch(RANDOM_QUOTE_URL);
+        const jsonResponse = await response.json();
+        const randomQuote = jsonResponse.quote;
+        setQuote(randomQuote);
+    }
+    useEffect(() => {
+        fetchQuote();
+    }, [])
+    
+    return (
+        <div className="m-5">
+            <button onClick={fetchQuote}>Get Verse</button>
+            <h1>{quote.text}</h1>
+            <h3>{quote.author}</h3>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-
+"use client"
+import VerseFetcher from "./components/verse-fetcher"
 export default function Home() {
   return (
     <div className="row justify-content-center">
       <div className="row m-0 p-0 h-25">
         <img style={{objectFit: "cover", height: "300px"}} src="/banner.jpeg" alt="" />
       </div>
-      
+      <VerseFetcher />
     </div>
   )
 }
