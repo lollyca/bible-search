9731eaba33e93ecfea1f5bb03f7c325558deb331
Lelo <heloisacmk@hotmail.com>
2023-07-24 14:22:23 -0700

    fetcher display the verse text

--- app/components/test-fetcher.tsx
+++ app/components/test-fetcher.tsx
@@ -1,15 +1,17 @@
 //king james de4e12af7f28f599-02
 import { useState } from "react";
 export default function TestFetcher() {
-    const [searchText, setTextSearch] = useState("love")
-    
+    const [searchText, setTextSearch] = useState("love");
+    const [versesArray, setVersesArray] = useState([]);
+
+
     const updateTextSearch = (evt) => {
         setTextSearch(evt.target.value);
     };
     console.log(searchText)
 
     async function fetchSomething() {
-        const offset = 10;
+        const offset = 300;
         const url = `https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-02/search?query=${searchText}&offset=${offset}`;
         const options = {
             method: 'GET',
@@ -21,16 +23,26 @@ export default function TestFetcher() {
         try {
             const response = await fetch(url, options);
             const result = await response.json();
-            console.log(result.data);
+            const verses = result.data.verses
+            console.log(verses);
+            setVersesArray(verses);
         } catch (error) {
             console.error(error);
         }
     }
 
+
     return (
         <div>
-            <h1>Pega o livro</h1><input type="text" placeholder="username" value={searchText} onChange={updateTextSearch} id="username"/>
+            <h1>Key Word:</h1>
+            <input type="text" placeholder="username" value={searchText} onChange={updateTextSearch} id="username" />
             <button onClick={fetchSomething}>Click</button>
+            <h3>Results:</h3>
+            <ul>
+                {versesArray.map((verse) => (
+                    <li key={verse.id}>{verse.text}</li>
+                ))}
+            </ul>
         </div>
     )
 }
