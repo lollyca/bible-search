fa6158eb512d9065a14617cc338e84293bf98748
Lelo <heloisacmk@hotmail.com>
2023-07-26 15:34:35 -0700

    a better solution for highlighting the word

--- app/components/basic-card.tsx
+++ app/components/basic-card.tsx
@@ -14,20 +14,31 @@ const bull = (
   </Box>
 );
 
-export default function BasicCard(props) {
+export default function BasicCard({ text, match, reference }: any) {
+
+  const parts = getHighlightedText(text, match);
+
+  function getHighlightedText(text: string, highlight: string) {
+    const parts = text.split(new RegExp(`(${highlight})`, 'gi'));
+    console.log(parts);
+    
+    return <span>{parts.map(part => part.toLowerCase() === highlight.toLowerCase() ? <span style={{ backgroundColor: 'yellow', padding: '1px' }}><b>{part}</b></span> : part)}</span>;
+  }
 
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          {props.reference}
+          {reference}
         </Typography>
         <Typography variant="body2">
-          {props.text.split(' ').map((x: string) =>
+          {/* {props.text.split(' ').map((x: string) =>
             x.includes(props.match)
               ? <><span style={{ backgroundColor: 'yellow', padding: '1px' }}>{x}</span>&nbsp;</>
               : x + ' '
-          )}
+          )} */}
+
+          {parts}
         </Typography>
       </CardContent>
     </Card>
