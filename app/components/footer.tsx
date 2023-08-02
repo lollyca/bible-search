233680b9408e21d629b9840608d422b79ce3c86e
Lelo <heloisacmk@hotmail.com>
2023-08-02 13:32:50 -0700

    footer is bigger and text is align and centralized

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,7 @@ import "../styles.css";
 
 export default function Footer() {
     return (
-        <div className="bg-brand text-center" style={{ fontFamily: "sans-serif" }}>
+        <div className="bg-brand d-flex justify-content-center align-items-center" style={{ fontFamily: "sans-serif", height: "3vh" }}>
             Bible Search 2023
         </div>
     );
