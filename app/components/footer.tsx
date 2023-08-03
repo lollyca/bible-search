0bc582eed85fec753419d6ae072428f0fc2ccc60
Lelo <heloisacmk@hotmail.com>
2023-08-02 18:38:55 -0700

    I'm tired i want to go home

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,7 @@ import "../styles.css";
 
 export default function Footer() {
     return (
-        <div className="bg-brand d-flex justify-content-center align-items-center footer-text" style={{ height: "3vh" }}>
+        <div className="bg-brand d-flex justify-content-center align-items-center footer-text py-1">
             Bible Search 2023
         </div>
     );
--- app/components/lets-go-search.tsx
+++ app/components/lets-go-search.tsx
@@ -1,45 +1,34 @@
-import { ChangeEvent, KeyboardEvent, useEffect, useState } from "react";
-import { Box, Button, Stack, TextField } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-
-import "../styles.css"
-
-import { BibleVersion, bibleVersionsList } from '../model/bible-versions';
-import { Verse } from "../model/verse";
-import { redirect } from 'next/dist/server/api-utils';
-
-
+import { bibleVersionsList } from '../model/bible-versions';
+import '../styles.css';
 
 const DEFAULT_BIBLE = bibleVersionsList.find(x => x.label.toLowerCase().includes('king james'));
-
-
 
 export default function LetsGoSearch() {
 
     return (
-        <div className="d-flex flex-column justify-content-center align-items-center h-100">
-            <div className="text-center pt-4 main-text" style={{ fontSize: "xxx-large" }}>Lelo</div>
 
-            <form action="/search">
-                <input className="d-none" type="text" name="bibleVersion" defaultValue={DEFAULT_BIBLE?.id} />
+        <div className="d-flex justify-content-center align-items-center h-100">
 
-                <div className="d-flex justify-content-center align-items-center pb-3">
-                    <Box
-                        component="div"
-                        sx={{
-                            '& > :not(style)': { m: 1, width: '50ch' },
-                        }}
-                    >
-                        <TextField id="outlined-basic" defaultValue="" label="Search" variant="filled" name="text"/>
-                    </Box>
-                    <Stack direction="row">
-                        <Button type="submit" sx={{ height: "56px" }} variant="contained" color="primary">
-                            <SearchOutlinedIcon />
-                        </Button>
-                    </Stack>
+            <div className="row w-100 justify-content-center">
+                <div className="col-xs-12 col-sm-8 col-md-6 col-xl-5">
+                    <div className="text-center pt-4 main-text" style={{ fontSize: "xxx-large" }}>Lelo</div>
+                    <form action="/search">
+                        <input className="d-none" type="text" name="bibleVersion" defaultValue={DEFAULT_BIBLE?.id} />
+
+                        <div className="d-flex justify-content-center align-items-center pb-3">
+                            <TextField id="outlined-basic" className="w-100 me-2" defaultValue="" label="Search" variant="filled" name="text" />
+                            <Button type="submit" sx={{ height: "56px" }} variant="contained" color="primary">
+                                <SearchOutlinedIcon />
+                            </Button>
+
+                        </div>
+                    </form>
                 </div>
-            </form>
+            </div>
+
         </div>
 
-    )
+    );
 }
