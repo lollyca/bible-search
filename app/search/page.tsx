048ad38633ede930daefda34c94420481a69a25c
Lelo <heloisacmk@hotmail.com>
2023-08-02 17:10:21 -0700

    Search input is responsive

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -48,23 +48,18 @@ export default async function Search({ searchParams }: SearchPageProps) {
 
     return (
         <div className="h-100 d-flex flex-column p-0">
-            <div style={{border: "1px solid #c3b9b970"}}>
+            <div style={{ border: "1px solid #c3b9b970" }}>
                 <form action="/search" method="GET">
-                    <div className="bg-brand">
-                        <div className="d-flex justify-content-center align-items-center p-3">
-                            <Box component="div"
-                                sx={{
-                                    '& > :not(style)': { m: 1, width: '50ch' },
-                                }}
-                            >
-                                <TextField name="text" defaultValue={text} label="Search" variant="filled" />
-                            </Box>
+                    <div className="bg-brand row justify-content-center">
+                        <div className="col-xs-12 col-sm-8 col-md-7 col-lg-6">
 
-                            <Stack direction="row" sx={{ height: "56px" }}>
-                                <Button type="submit" variant="contained" color="primary">
+                            <div className="d-flex p-3">
+                                <TextField name="text" defaultValue={text} label="Search" variant="filled" className="me-2 w-100" />
+                                <Button style={{ height: '56px' }} type="submit" variant="contained" color="primary">
                                     <SearchOutlinedIcon />
                                 </Button>
-                            </Stack>
+                            </div>
+
                         </div>
                     </div>
                     <div className="d-flex justify-content-center align-items-center">
